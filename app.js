01e7d2b2ec38bea44ae3d39f39ef603d503a5148
shadowdeveloper <ro_web1@hotmail.com>
2017-12-05 22:41:55 -0200

    route config

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ var dependecies = [
   'ngRoute'
 ];
 
-
 //App module
 var app = angular.module('app', dependecies);
 
-console.log(app);
+app.config(['$routeProvider', '$locationProvider', routeManager]);
+
+function routeManager($routeProvider, $locationProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl: './partials/01 - HelloWorld/index.html',
+      controller: 'firstController'
+    }).when('/second', {
+      templateUrl: './partials/02 - Lista_telefonica/index.html',
+      controller: 'secondController'
+    });
+  // $locationProvider.html5mode(false).hashPrefix('!');
+}
